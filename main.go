a5dfc244b48dfe9a5e9337a61e50ee7fcec9c9ee
komrakov <alexkomrakov@gmail.com>
2016-11-22 12:41:51 +0300

    + initital commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/elazarl/goproxy"
+	"log"
+	"flag"
+	"net/http"
+)
+
+func main() {
+	verbose := flag.Bool("v", false, "should every proxy request be logged to stdout")
+	addr := flag.String("a", ":5555", "proxy listen address")
+	flag.Parse()
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = *verbose
+	log.Println("Server started")
+	log.Fatal(http.ListenAndServe(*addr, proxy))
+}
